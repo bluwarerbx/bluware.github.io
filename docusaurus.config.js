43ab0f66b19abc01d578b9bddee0cc5b13f69e21
bizzo <bizlol2006@gmail.com>
2024-03-31 22:06:12 +0100

    xs

---
+++ docusaurus.config.js
@@ -0,0 +1,8 @@
+export default {
+    title: 'Docusaurus',
+    url: 'https://docusaurus.io',
+    // your site config ...
+    baseUrl: /bluware.github.io/
+  };
+  
+  baseUrl = /bluware.github.io/
